378bd7a009204b599da42ca693a4da566e10c0c4
Zach Tirrell <zbtirrell@gmail.com>
2013-08-28 08:30:16 -0400

    updating comment header block

--- go-taxonomy.php
+++ go-taxonomy.php
@@ -1,14 +1,13 @@
 <?php
-/*
-Plugin Name: GigaOM Taxonomy Registration
-Plugin URI: http://gigaom.com/
-Description: Registers taxonomies based on data from go-config
-Version: 0.1
-Author: GigaOM <support@gigaom.com>
-Author URI: http://gigaom.com/
-License: All Rights Reserved.
-*/
+/**
+ * Plugin Name: Gigaom Taxonomy Registration
+ * Plugin URI: http://gigaom.com/
+ * Description: Registers taxonomies based on data from go-config
+ * Version: 0.1
+ * Author: Gigaom <support@gigaom.com>
+ * Author URI: http://gigaom.com/
+ * License: All Rights Reserved.
+ */
 
 require_once __DIR__ . '/components/class-go-taxonomy.php';
-
 go_taxonomy();
