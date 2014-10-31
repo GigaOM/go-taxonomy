9be56acf3cd300544da2b9edb9806b4276a0af9c
Jamie Poitra <jamie@gigaom.com>
2014-10-31 23:37:09 +0000

    Adding taxonomy hiding stuff to go-taxonomy

--- components/class-go-taxonomy.php
+++ components/class-go-taxonomy.php
@@ -9,6 +9,7 @@ class GO_Taxonomy
 		$this->config( apply_filters( 'go_config', false, 'go-taxonomy' ) );
 
 		add_action( 'init', array( $this, 'init' ), 1 );
+		add_action( 'registered_taxonomy', array( $this, 'registered_taxonomy' ) );
 		add_filter( 'the_category_rss', array( $this, 'the_category_rss' ), 10, 2 );
 		add_filter( 'go_taxonomy_sorted_terms', array( $this, 'sorted_terms_filter' ), 1, 3 );
 	}//end __construct
@@ -33,6 +34,20 @@ class GO_Taxonomy
 	{
 		$this->register();
 	}//end init
+
+	/**
+	 * Hook into the registered_taxonomy action and do things on a per taxonomy basis (like hide a taxonomy)
+	 */
+	public function registered_taxonomy( $taxonomy )
+	{
+		global $wp_taxonomies;
+
+		// Hide taxonomies
+		if ( in_array( $taxonomy, $this->config['taxonomies_to_hide'] ) )
+		{
+			$wp_taxonomies[ $taxonomy ]->show_ui = FALSE;
+		} // END if
+	} // END registered_taxonomy
 
 	/**
 	 * register taxonomies based on config data
